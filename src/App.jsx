11dfedcc5496bfb4b6d7bd34ca56fecd099b712d
BricAppBrac <sandrinesteiger3000@gmail.com>
2025-11-21 16:51:49 +0100

    V0.2 Ajout du Bouton Home pour réinitialiser

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,30 @@
 // App.jsx
+import { useState } from "react";
 import UploadRevueAnalytique from "./components/UploadRevueAnalytique";
 
 function App() {
+  // Clé pour forcer la réinitialisation du composant enfant
+  const [resetKey, setResetKey] = useState(0);
+
+  // Bouton Home : réinitialise l'appli
+  const handleHome = () => {
+    setResetKey((prev) => prev + 1);  // change la key -> remount
+    window.scrollTo({ top: 0, behavior: "smooth" }); // optionnel
+  };
+
   return (
     <div className="home">
       <div className="home-title">
         <h1>REVUE ANALYTIQUE</h1>
-        
-      {/* éventuellement un bouton "maison" plus tard */}
+         <button onClick={handleHome}>
+          <i className="fa-solid fa-house"></i>
+        </button>
+      
       </div>
 <h2>******************************************</h2>
       <div className="home-content">
-        <UploadRevueAnalytique />
+        {/* key = resetKey permet de repartir à zéro quand on clique sur la maison */}
+        <UploadRevueAnalytique key={resetKey} />
       </div>
     </div>
   );
